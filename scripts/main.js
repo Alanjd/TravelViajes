de902c7a63519dced529ae151fbc210bb5df07dd
Alan Jaime <alandario9016@gmail.com>
2022-03-11 16:27:53 -0300

    modificaciones js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,53 @@
-const listaDestinos = [ DesBarcelona, desBali, desBariloche, desBrasil]
+const destinos = [{id: 1, nombre: "Barcelona", precio: 250000 },
+                  {id: 1, nombre: "Barcelona", precio: 250000 },
+                  {id: 1, nombre: "Barcelona", precio: 250000 },
+                  {id: 1, nombre: "Barcelona", precio: 250000 }
+];
 
-let categoriaDestino = prompt ()
+
+for (const destino of listaDestinos){
+ let contenedor = document.createElement("div");
+                
+        contenedor.innerHTML = `<h3> ID: ${destino.id}<h3>
+                                <p> Destino: ${destino.nombre}<p>
+                                <b> Precio:$ ${destino.precio}<b>`;
+        catalogo.appendChild(contenedor);
+}
+
+
+const destinoBarcelona = new viaje ("Barcelona", 20 , 250000, 1 );
+const destinoBali = new viaje ("Bali", 10 , 315000, 2 );
+const destinoBariloche = new viaje ("Bariloche", 60 , 15000, 3 );
+const destinoBrasil = new viaje ("Brasil", 50 , 120000, 4 );
+const destinoMaldivas = new viaje ("Maldivas", 30 , 400000);
+const destinoMexico = new viaje ("Mexico", 40 , 170000);
+const destinoParis = new viaje ("Paris", 20 , 260000);
+const destinoTurquia = new viaje ("Turquia", 15 , 210000); 
+
+
+const listaDestinos = [ destinoBarcelona, destinoBali, destinoBariloche, destinoBrasil]
+
+let botonDestinos = document.querySelector('.botonDestinos')
+
+botonDestinos.addEventListener('click', destinos)
+
+let catalgo = document.querySelector('.catalogo')
+
+function destinos(){
+    
+    for (const destino of listaDestinos){
+    let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = `<h3> ID: ${destino.id}<h3>
+                            <p> Destino: ${destino.nombre}<p>
+                            <b> Precio:$ ${destino.precio}<b>`;
+    catalogo.appendChild(contenedor);
+    }
+}
+
+
+
+/*let categoriaDestino = prompt ()
 for (const destino of destinos){
     let contenedor = document.createElement("div");
 
@@ -24,14 +71,6 @@ function viaje (nombre, stock, precio){
 }
 
 
-const destinoBarcelona = new viaje ("Barcelona", 20 , 250000 , 1 , "./img/barcelona.jpg");
-const destinoBali = new viaje ("Bali", 10 , 315000);
-const destinoBariloche = new viaje ("Bariloche", 60 , 15000);
-const destinoBrasil = new viaje ("Brasil", 50 , 120000);
-const destinoMaldivas = new viaje ("Maldivas", 30 , 400000);
-const destinoMexico = new viaje ("Mexico", 40 , 170000);
-const destinoParis = new viaje ("Paris", 20 , 260000);
-const destinoTurquia = new viaje ("Turquia", 15 , 210000); 
 
 const arrayDestinos = [destinoBarcelona, destinoBali, destinoBariloche, destinoBrasil, destinoMaldivas, destinoMexico, destinoParis, destinoTurquia];
 const carrito = [];
